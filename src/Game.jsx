751c7177dc49ec124e9076f1307fb66b2aadf340
FSoprano <nel.cono@outlook.com>
2021-05-30 19:15:12 +0200

    Add PropTypes, props to Game.jsx / set state outside of constructor

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,21 +1,28 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import Card from './Card';
 import Animal from './Animal';
 import './Game.css';
 
 export default class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+  static defaultProps = {
+    title: 'Supertrumpf',
+    };
+  static propTypes = {
+    title: PropTypes.string,
+    };
+  // Der State kann inzwischen auch außerhalb des contructors gesetzt 
+  // werden.
+  state = {
       playersTurn:true,
       player: [new Animal('Elefant', 'placeholder.png', 3.3, 6000, 70, 1, 40)],
       computer: [new Animal('Nashorn', 'placeholder.png', 1, 9, 2300, 50, 1, 50)],
       }
-    }
   render() {
     const { playersTurn, player, computer } = this.state;
     return (
       <div>
+        <h1>{this.props.title}</h1>
         <div className="info">
           {playersTurn ? 'Du bist' : 'Der Computer ist'} an der Reihe.
         </div>
