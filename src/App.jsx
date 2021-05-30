3b173c4c33ad95320e73da303a4efb2d2b887eb6
FSoprano <nel.cono@outlook.com>
2021-05-30 19:00:04 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './App.css';
+import Game from './Game';
+
+export default function App() {
+  return <Game title="Supertrumpf" />;
+}
---
+++ src/Card.jsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import './Card.css';
+import Animal from './Animal';
+
+export default function Card({ animal, uncovered }) {
+  Card.propTypes = {
+    uncovered: PropTypes.bool.isRequired,
+    animal: PropTypes.instanceOf(Animal).isRequired,
+  };
+  const front = (
+    <div className="card">
+      <h1>{animal.name ? animal.name : 'Unbekannt'}</h1>
+      {/* Der gute alte Ternär-Operator. */}
+      {animal.image && (
+      <img
+        alt={animal.name}
+        height="200"
+        width="200"
+        src={`${process.env.PUBLIC_URL}/${animal.image}`}
+      />
+      )}
+      {/* Zusätzliches Sicherheitsgedödel. Die beiden Zeilen
+              oben sind nichts weiter als eine verkürzte if-Bedingung
+              Nur wenn für elephant.image ein Wert zurückgegeben kann,
+              wird das Bild angezeigt. */}
+      <table>
+        <tbody>
+          {Object.keys(Animal.properties).map((property) => {
+            const animalProperty = Animal.properties[property];
+            return (
+              <tr key={property}>
+                <td>{animalProperty.label}</td>
+                <td>
+                  {animal[property]} &nbsp;
+                  {animalProperty.unit}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+  const back = <div className="card back" />;
+  if (uncovered) {
+    return front;
+  }
+  return back;
+}
